69fb0da77920faec44b11b4b108e96cfc5e8ecc5
Ivan Wills <ivan.wills@gmail.com>
2011-12-09 22:00:08 +1100

    Added empty lib for build purposes

---
+++ lib/App/Useful.pm
@@ -0,0 +1,110 @@
+package App::Useful;
+
+# Created on: 2011-12-09 21:40:57
+# Create by:  Ivan Wills
+# $Id$
+# $Revision$, $HeadURL$, $Date$
+# $Revision$, $Source$, $Date$
+
+use strict;
+use warnings;
+use version;
+
+our $VERSION     = version->new('0.0.1');
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::Useful - Package to provide useful commands
+
+=head1 VERSION
+
+This documentation refers to App::Useful version 0.1.
+
+=head1 SYNOPSIS
+
+    $ bigfiles
+    $ build-all
+    $ builder
+    $ chkfacl
+    $ cmdaliaser
+    $ coder
+    $ csv2html-table.pl
+    $ devmode
+    $ devremote
+    $ df-colour
+    $ diffdir
+    $ duhs
+    $ duuser
+    $ everything
+    $ fdiff
+    $ gitrev
+    $ highlight
+    $ hlist
+    $ hours
+    $ html-cleaner
+    $ httprecorder
+    $ img-resize
+    $ img-size
+    $ itop
+    $ myget
+    $ ossh
+    $ partition-table.pl
+    $ pingfind
+    $ player-alarm
+    $ player-pause.pl
+    $ posture
+    $ psqlout2csv.pl
+    $ rhythmbox-ctl.pl
+    $ schema-loader
+    $ set-title
+    $ spchk
+    $ ssh-to
+    $ stop-watch
+    $ suggest-tests
+    $ svntouch.pl
+    $ swapfiles
+    $ used
+    $ v
+    $ validate_json
+    $ vims
+
+=head1 DESCRIPTION
+
+=head1 SUBROUTINES/METHODS
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.
+
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+Ivan Wills - (ivan.wills@gmail.com)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2011 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+=cut
